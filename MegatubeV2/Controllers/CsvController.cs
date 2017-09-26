f257b46f9a174e459fdc57bc42c8735b6d19202e
Alessio Greco <alxeyesoul@live.com>
2017-09-26 16:40:32 +0200

    Upload file, App_Code folder (with placeholder) HTML upload elements

--- MegatubeV2/Controllers/CsvController.cs
+++ MegatubeV2/Controllers/CsvController.cs
@@ -8,14 +8,28 @@ namespace MegatubeV2.Controllers
 {
     public class CsvController : Controller
     {
-        // GET: Csv
+        private MegatubeV2Entities db = new MegatubeV2Entities();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        //public ActionResult UploadChannels(HttpPostedFileBase upload)
-        //{
-        //}
+        //YouTube_id0iPcHVVMh4ASTXBUYkOA_Ecommerce_paid_features_M_20170801_20170831_v1-0
+        //YouTube_id0iPcHVVMh4ASTXBUYkOA_M_20170801_20170831_videoclaim_rawdata_v1-0
+        public ActionResult UploadChannels(HttpPostedFileBase file, float dollarToEuro)
+        {
+            return null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
     }
 }
