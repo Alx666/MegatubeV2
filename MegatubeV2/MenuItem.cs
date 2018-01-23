c0bf240f48a3b376c241e0af4297103804bb406e
chirone0 <chirone0@gmail.com>
2018-01-23 14:53:19 +0100

    new menu

---
+++ MegatubeV2/MenuItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegatubeV2
+{
+    public class MenuItem
+    {
+        public string Name { get; protected set; }
+        public string Icon { get; protected set; }
+        public string ActionName { get; protected set; }
+        public string ControllerName { get; protected set; }
+        public bool HasChildren { get { return Children.Count > 0; } }
+        public List<MenuItem> Children;
+
+        public MenuItem(string name, string action, string controller, string icon="")
+        {
+            Name = name;
+            Icon = icon;
+            ActionName = action;
+            ControllerName = controller;
+
+            Children = new List<MenuItem>();
+        }
+
+        public void SetChildred(List<MenuItem> childrenList)
+        {
+            Children = new List<MenuItem>(childrenList);
+        }
+
+        public void AddChild(MenuItem child)
+        {
+            Children.Add(child);
+        }
+
+        public bool ContainsChild(MenuItem child)
+        {
+            return Children.Contains(child);
+        }
+
+        public bool ChildMatches(string controller, string action)
+        {
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].ActionName == action && Children[i].ControllerName == controller)
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
